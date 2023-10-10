4d6cdfc097c38a926df4d9e496af5c274bc35e8e
durand225 <140192544+durand225@users.noreply.github.com>
2023-10-10 16:51:37 +0000

    add

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
 
 
-@app.route('acceuil/')
-def ikndex():
-    pass
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET','POST'])
+def accueil():
+    return render_template('/html/connexion.html')
+
+@app.route('/aboute')
+def about_us():
+    return render_template('/html/index.html')
+
+
+@app.route('/acceuil')
+def acceuil():
+    return render_template('/html/index.html')
+
+
